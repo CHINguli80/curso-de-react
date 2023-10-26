ca01592e9f4ffe4173fa6400e59384a443b153e8
Morais Domingos <141407881+CHINguli80@users.noreply.github.com>
2023-10-26 14:40:58 +0100

    this.setState em classes

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,121 +1,28 @@
-import { /* Component */ useEffect, useState, useCallback } from 'react'
+import { Component } from 'react'
 import './style.css';
-import { PostCard } from '../../components/PostCard';
-import { loadPosts } from '../../utils/load';
-import { Button } from '../../components/Button';
-import { TextInput } from '../../components/TextInput';
 
-export const Home = () => {
-  const [posts, setPosts] = useState([])
-  const [allPosts, setAllPost] = useState([])
-  const [page, setPage] = useState(0)
-  const [postsPerpage] = useState(10)
-  const [searchValue, setSearchValue] = useState('')
-  
-  const noMorePosts = page + postsPerpage >= allPosts.length
+export class Home extends Component {
+    state = {
+      counter: 0
+    }
 
-  const filterPosts = !!searchValue ? 
-    allPosts.filter(post => {
-      return post.title.toLowerCase().includes(
-        searchValue.toLowerCase()
+    handleClick = () => {
+        this.setState(
+          (prevState, prevProps) => {
+            return { counter: prevState.counter + prevProps.numberIncrement}
+          },
+          () => {
+            console.log('POST', this.state.counter)
+          }
+        )  
+    }
+
+    render() {
+      return (
+        <div className='container'>
+          <h1>{this.state.counter}</h1>
+          <button onClick={this.handleClick}>Increment</button>
+        </div>
       )
-    })
-    : posts;
-
-
-    const handleLoadPosts = useCallback(async (page, postsPerpage) => {
-      const postsAndPhotos = await loadPosts()  
-
-      setPosts(postsAndPhotos.slice(page, postsPerpage))
-      setAllPost(postsAndPhotos)
-    }, [])
-
-    useEffect(() => {
-      handleLoadPosts(0, postsPerpage)
-    }, [handleLoadPosts, postsPerpage])
-  
-    const loadMorePosts = () => {
-      const nextPage = page + postsPerpage
-      const nextPosts = allPosts.slice(nextPage, nextPage + postsPerpage)
-      posts.push(...nextPosts)
-  
-      setPosts(posts)
-      setPage(nextPage)
-  
     }
-  
-    const handleChange = (e) => {
-        const {value} = e.target
-        setSearchValue(value)
-    }
-    
-  return (
-    <section className='container'>
-    
-       <div className='search-container'>
-           {!!searchValue && (
-           <h1>Pesquisou por: {searchValue}</h1>
-         )}
-
-         <TextInput 
-         searchValue={searchValue} 
-         handleChange={handleChange}
-         />
-       </div>
-    
-        <div className="posts">
-      
-        {filterPosts.length > 0 && (
-       <>
-        {
-         filterPosts.map(post => (
-           <PostCard 
-           key={post.id}
-             post={post}
-           />
-           ))
-         }
-         </>
-     )}
-
-         {filterPosts.length === 0 && (
-           <p>Não existem Posts</p>
-         )}
-
-       <div className='button-container'>
-         {!searchValue && (
-           <Button 
-           text="Lore more Post"
-           onclick={loadMorePosts}
-           disabled={noMorePosts}
-        />
-         )}
-       
-       </div>
-     </div>
-    </section>
-   );
-  
 }
-
-
-/* export class Home2 extends Component {
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postsPerpage: 12,
-    searchValue: ''
-  }
-
- async componentDidMount() {
-    await this.loadPosts()   
-  }
-
-  render() {
-    const { posts, page, postsPerpage, allPosts, searchValue } = this.state
-
-    
-  }
-  
-} */
